aa21611ad0548ee40e7bd1f4e2abda22ddf951e5
unknown <mahmoudezz878@gmail.com>
2021-08-05 02:01:04 +0200

    renew

--- src/Compnents/Channelrow/index.js
+++ src/Compnents/Channelrow/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Avatar from "@material-ui/core/avatar";
+import { Avatar } from '@material-ui/core';
 import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
 
 import { makeStyles } from "@material-ui/core/styles";
